82abec435814160a678043085015d28319c6c161
DiAnonim <akopyan.2002@mail.ru>
2023-11-28 00:34:38 +0600

    Добавьте файлы проекта.

---
+++ Practice_Coding/Duplicate_Encoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Practice_Coding
+{
+    internal class Duplicate_Encoder
+    {
+        public string newStr = string.Empty;
+
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
