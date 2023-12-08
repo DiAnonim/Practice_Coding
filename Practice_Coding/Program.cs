7a4001f166416ca8d5aa98760a203b5cba4d996c
DiAnonim <akopyan.2002@mail.ru>
2023-12-08 13:49:36 +0600

    Доделала Карты

--- Practice_Coding/Program.cs
+++ Practice_Coding/Program.cs
@@ -16,6 +16,12 @@ namespace Practice_Coding
     internal class BlackJack
     {
         public static Hashtable masCard = new Hashtable() {
+            { 0, "0" },
+            { 1, "Ace" },
+            { 2, "Two" },
+            { 3, "Three" },
+            { 4, "Four" },
+            { 5, "Five" },
             { 6, "Six" },
             { 7, "Seven" },
             { 8, "Eight" },
@@ -28,69 +34,106 @@ namespace Practice_Coding
        };
         static void Main(string[] args)
         {
-            Card card = new Card("Six", 6);
-            Console.WriteLine($"{card.Name}-{card.number}");
+            Card card = new Card("Ten", 10, "a");
+            Card card2 = new Card("Seven", 7, "a");
+            //Console.WriteLine($"{card.name}-{card.number}");
             //card--;
-            Console.WriteLine($"{card.Name}-{card.number}");
+            //Console.WriteLine($"{card.name}-{card.number}");
+
+            //Console.WriteLine($"{card.name}-{card.number}-{card.suit}");
+            //Console.WriteLine($"{card2.name}-{card2.number}-{card2.suit}");
+
+            //Console.WriteLine(card < card2);
+
+            Card c3 = card2 + card;
+            Console.WriteLine($"{c3.name}-{c3.number}-{c3.suit}");
 
             Console.ReadKey();
         }
 
         public class Card
         {
-            public string Name;
+            public string name;
             public int number;
+            public string suit;
 
-            public Card(string _name, int _number)
+
+            public Card(string _name, int _number, string _suit)
             {
-                Name = _name;
+                name = _name;
                 number = _number;
+                suit = _suit;
             }
 
             public static Card operator ++(Card c1)
             {
                 if (c1.number < 14)
-                    c1.Name = masCard[++c1.number].ToString();
+                    c1.name = masCard[++c1.number].ToString();
                 return c1;
             }
 
             public static Card operator --(Card c1)
             {
-                if (c1.number == 6)
-                {
-                    c1.number = 14;
-                    c1.Name = masCard[c1.number].ToString();
-                }
-                else if (c1.number > 6)
-                    c1.Name = masCard[--c1.number].ToString();
+                //if (c1.number == 6)
+                //{
+                //    c1.number = 14;
+                //    c1.Name = masCard[c1.number].ToString();
+                //}
+                //else if (c1.number > 6)
+                c1.name = masCard[--c1.number].ToString();
                 return c1;
             }
 
-            //public static Card operator += (Card c1, Card c2)
-            //{
-            //    if (c1.number == 6)
-            //    {
-            //        c1.number = 14;
-            //        c1.Name = masCard[c1.number].ToString();
-            //    }
-            //    else if (c1.number > 6)
-            //        c1.Name = masCard[--c1.number].ToString();
-            //    return c1;
-            //}
+            public static Card operator -(Card c1, Card c2)
+            {
+                if (c1.suit == c2.suit)
+                {
+                    int temp = c1.number - c2.number;
+                    if (temp > 0)
+                        return new Card(masCard[temp].ToString(), temp, c1.suit);
+                    else
+                        return new Card(masCard[0].ToString(), 0, "Число карты стало меньше 0");
+                }
+                else return new Card(masCard[0].ToString(), 0, "Не совпадают масти");
+            }
 
+            public static Card operator +(Card c1, Card c2)
+            {
+                if (c1.suit == c2.suit)
+                {
+                    int temp = c1.number + c2.number;
+                    if (temp <= 14) 
+                        return new Card(masCard[temp].ToString(), temp, c1.suit);
+                    else
+                        return new Card(masCard[0].ToString(), 0, "Число карты стало превышать 14");
+                }
+                else return new Card(masCard[0].ToString(), 0, "Не совпадают масти");
+            }
 
-            //public static bool operator >(Card a, Card b)
-            //{
-            //    if (a.Name == "Queen" && b.Name == "Jack")
-            //    {
-            //        return true;
-            //    }
-            //    return false;
-            //}
-            //public static bool operator <(Card a, Card b)
-            //{
-            //    return false;
-            //}
+            public static bool operator >(Card c1, Card c2)
+            {
+                string trump = "diamonds";
+                if (c1.number > c2.number && c1.suit == c2.suit)
+                    return true;
+                else if (c1.number < c2.number && c1.suit == trump && c2.suit != trump)
+                    return true;
+                else if (c1.number > c2.number && c1.suit != c2.suit && c2.suit != trump)
+                    return true;
+                return false;
+            }
+
+            public static bool operator <(Card c1, Card c2)
+            {
+                string trump = "diamonds";
+                if (c1.number < c2.number && c1.suit == c2.suit)
+                    return true;
+                else if (c1.number > c2.number && c1.suit != trump && c2.suit == trump)
+                    return true;
+                else if (c1.number < c2.number && c1.suit != c2.suit)
+                    return true;
+                return false;
+            }
+
         }
     }
 }
